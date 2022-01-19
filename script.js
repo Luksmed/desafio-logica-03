ba6a7a3543fe6ff1a7c5304767bd5154f7fc93a0
Luksmed <lukksme@gmail.com>
2022-01-18 22:44:10 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+let conta = 10
+while(1 <= conta){
+    console.log(conta)
+    conta --
+}
+
+for(let a = 1; a <= 100; a +=2){
+    console.log(a)
+}
+
+for(let b = 0; b <= 100; b +=2){
+    console.log(b)
+}
